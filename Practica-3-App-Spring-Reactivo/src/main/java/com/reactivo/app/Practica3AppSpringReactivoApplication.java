fd3d5a8d3c52f7647e5604bc63616cbe3f1e84d5
lizegira <lizegira@bancolombia.com.co>
2022-03-16 15:10:07 -0500

    App Spring reactivo structure

--- Practica-3-App-Spring-Reactivo/src/main/java/com/reactivo/app/Practica3AppSpringReactivoApplication.java
+++ Practica-3-App-Spring-Reactivo/src/main/java/com/reactivo/app/Practica3AppSpringReactivoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Practica3AppSpringReactivoApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Practica3AppSpringReactivoApplication.class, args);
 	}
